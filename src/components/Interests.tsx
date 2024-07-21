ccefa4627beec96b2bbe3d1e35f1988d7fbdaa8b
Yash Goyal <128176081+YashGoyal9@users.noreply.github.com>
2024-07-21 18:03:09 +0530

    Update Interests.tsx

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -5,23 +5,23 @@ import Image from "next/image";
 import mateSC from "@/fonts/MateScFont";
 import Link from "next/link";
 
-// cricket,designning,music ,poetry
+// cricket, designing, music, poetry
 const content = [
   {
     title: "Designing",
     description:
-      "I have a keen interest in designing, and I enjoy exploring various aspects of it. From canva designing to user experience, I am passionate about creating visually appealing designs.",
+      "I have a keen interest in designing, and I enjoy exploring various aspects of it. From Canva designing to user experience, I am passionate about creating visually appealing designs.",
     content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-       <Link href="https://drive.google.com/drive/folders/14UoPJwiJoi9uGNB5pSdD41R5iWV3oVWW" passHref>
-       <Image
-          src="/assessts/designer.png"
-          width={300}
-          height={400}
-          className="h-full w-full object-cover"
-          alt="linear board demo1"
-        />
-       </Link>
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <Link href="https://drive.google.com/drive/folders/14UoPJwiJoi9uGNB5pSdD41R5iWV3oVWW" passHref>
+          <Image
+            src="/assessts/designer.png"
+            width={300}
+            height={400}
+            className="h-full w-full object-cover"
+            alt="design"
+          />
+        </Link>
       </div>
     ),
   },
@@ -30,34 +30,34 @@ const content = [
     description:
       "I have a keen interest in listening to music, finding it a great source of relaxation and inspiration. Whether it's exploring new genres or enjoying my favorite tracks, music is an integral part of my life.",
     content: (
-      <div className="h-full w-full  flex  items-center justify-center text-white">
-       <Link href="https://open.spotify.com/playlist/0IhzydvhkIVghb0hFONTG7?si=KOV_Ec0cSfOZFx495vlgrA" passHref>
-       <Image
-          src="/assessts/music.jpeg"
-          width={300}
-          height={300}
-          className="h-full w-full object-cover"
-          alt="linear board demo2"
-        />
-       </Link>
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <Link href="https://open.spotify.com/playlist/0IhzydvhkIVghb0hFONTG7?si=KOV_Ec0cSfOZFx495vlgrA" passHref>
+          <Image
+            src="/assessts/music.jpeg"
+            width={300}
+            height={300}
+            className="h-full w-full object-cover"
+            alt="music"
+          />
+        </Link>
       </div>
     ),
   },
   {
     title: "Cricket",
     description:
-"I have a keen interest in playing cricket, and he, the legend, is my inspiration. His leadership and skill always help me to improve my life.",
+      "I have a keen interest in playing cricket, and MS Dhoni, the legend, is my inspiration. His leadership and skill always help me to improve my life.",
     content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-       <Link href="https://www.instagram.com/mahi7781?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" passHref>
-       <Image
-          src="/assessts/msd.jpg"
-          width={300}
-          height={300}
-          className="h-full w-full object-cover"
-          alt="linear board demo3"
-        />
-       </Link>
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <Link href="https://www.instagram.com/mahi7781" passHref>
+          <Image
+            src="/assessts/msd.jpg"
+            width={300}
+            height={300}
+            className="h-full w-full object-cover"
+            alt="msd"
+          />
+        </Link>
       </div>
     ),
   },
@@ -66,25 +66,24 @@ const content = [
     description:
       "I have a keen interest in writing and hearing poetry (shayari). The beauty of words and emotions in shayari deeply resonates with me and inspires my own creative expression.",
     content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-        <Link href="https://www.instagram.com/yahyabootwala?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" passHref>
-        <Image
-          src="/assessts/poetry.png"
-          width={300}
-          height={400}
-          className="h-full w-full object-cover"
-          alt="linear board demo4"
-        />
+      <div className="h-full w-full flex items-center justify-center text-white">
+        <Link href="https://www.instagram.com/yahyabootwala" passHref>
+          <Image
+            src="/assessts/poetry.png"
+            width={300}
+            height={400}
+            className="h-full w-full object-cover"
+            alt="poetry"
+          />
         </Link>
       </div>
     ),
   },
   {
     title: "",
-    description:
-        "",
+    description: "",
     content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
+      <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src=""
           width={300}
@@ -100,16 +99,14 @@ const content = [
 export function Interests() {
   return (
     <div>
-        <div className="pt-24 text-5xl md:text-6xl  bg-clip-text text-transparent dark:bg-white bg-black 
-            text-center  font-extrabold">
+      <div className="pt-24 text-5xl md:text-6xl bg-clip-text text-transparent dark:bg-white bg-black text-center font-extrabold">
         <h1 className={mateSC.className}>
-            Interests
-      </h1>
-    </div>
-
-    <div className="p-10">
-      <StickyScroll content={content} />
-    </div>
+          Interests
+        </h1>
+      </div>
+      <div className="p-10">
+        <StickyScroll content={content} />
+      </div>
     </div>
   );
 }
